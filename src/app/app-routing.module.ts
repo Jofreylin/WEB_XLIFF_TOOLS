9d2376a633da3f845a21ad513fd9caa4228efcdd
Jofreylin Perez Valdez <jperezv@isf.do>
2024-08-02 10:55:44 -0400

    Added first libraries and tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: CommonLayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeEn from '@angular/common/locales/en';
+import { HomeComponent } from './pages/home/home.component';
+import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
+import { SharedModule } from './shared/shared.module';
+
+registerLocaleData(localeEn, 'en-US');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CommonLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgxSpinnerModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withFetch()),
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'en-US' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
